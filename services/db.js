f9030ab91dce8b1503fd082532afe663f9ff9ec2
1760023-Hoàng Nguyễn Đại <hoangdai199900@gmail.com>
2020-08-01 19:15:10 +0700

    1760023
    1760023

--- services/db.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connectionString = process.env.DATABASE_URL ||'postgres://postgres:953093@localhost:5432/InternetBanking';
-const db =new Sequelize(connectionString)
-
-module.exports=db;
