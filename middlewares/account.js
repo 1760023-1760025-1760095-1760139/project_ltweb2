897a3ad12e402aaea188ad22c0f5b901a0dc134e
Hong Lien <58362687+honglien82@users.noreply.github.com>
2020-08-12 14:15:22 +0700

    Update account.js

--- middlewares/account.js
+++ middlewares/account.js
@@ -1,7 +1,8 @@
 const Account=require('../services/account');
 const asyncHandler=require('express-async-handler');
 
-module.exports= asyncHandler(async function auth(req,res,next){
+module.exports= asyncHandler(async function auth(req,res,next)
+{
     const userId=req.session.userId;
     res.locals.currentAccount=null;
     if(!userId){
